ccc6011f2398c5fbecbd57153dfa2a6fb793c001
Yiğithan Fırat <103052898+Abusivesnake@users.noreply.github.com>
2023-12-01 19:40:29 +0300

    Update atm.c
    Para Çekme işlemi sonrasında başka işlem yapılıp yapılmama durumunu kontrol ediliyor, işlem yapılmayacaksa sistem bitirilir.
    
    İşlem yapılacaksa hesap numarası değiştirip değiştirilmeyeceği soruluyor. Hesap numarası değişmeyecekse aynı hesap numarasından farklı işlem menüsüne yönlendiriliyor.
    
    Hesap numarası değişecekse yeni hesap numarasıyla yeni yapmak istediği işlem bilgileri alınıp bu Islemler() fonksiyonuna gönderiliyor ve bu bilgiler üzerinden hareket ediliyor.

--- atm.c
+++ atm.c
@@ -22,6 +22,85 @@ int Islemler(int hesapno, int islemID)
                 scanf("%d", &para);
             }
             printf("[SİSTEM]: %d numaralı hesap numarasından %d miktarında para çekildi!", hesapno, para);
+            int secim;
+            printf("[SİSTEM]: Başka bir işlem yapmak ister misiniz? (0-1)");
+            scanf("%d", &secim);
+            switch(secim)
+            {
+                case 0:
+                {
+                    printf("[SİSTEM]: Sistemden çıkış yapılıyor...\n");
+                    printf("[UYARI]: Kartınızı hazneden teslim alınız!\n");
+                    printf("[SİSTEM]: Sistem çıkışı yapıldı!\n");
+                    return 1;
+                    break;
+                }
+                case 1:
+                {
+                    int option;
+                    printf("[SİSTEM]: Hesap numarası değiştirmek ister misiniz? (0-1)");
+                    scanf("%d", &option);
+                    switch(option)
+                    {
+                        case 0:
+                        {
+                            int yeniislemID;
+                            printf("1 - Para Çekme\n");
+                            printf("2 - Para Yatırma\n");
+                            printf("3 - Para Transferi\n");
+                            printf("4 - Sistemden Çıkış\n");
+                            printf("[SİSTEM]: Yapmak istediğiniz işlemi seçiniz: ");
+                            scanf("%d", &yeniislemID);
+                            while(yeniislemID < 1 || yeniislemID > 4)
+                            {
+                                printf("[HATA]: Hatalı işlem ID girdiniz!");
+                                printf("[BİLGİ]: İşlem ID'lerimiz 1-4 arasındadır!");
+                                printf("1 - Para Çekme\n");
+                                printf("2 - Para Yatırma\n");
+                                printf("3 - Para Transferi\n");
+                                printf("4 - Sistemden Çıkış\n");
+                                printf("[SİSTEM]: Yapmak istediğiniz işlemi seçiniz: ");
+                                scanf("%d", &yeniislemID);
+                            }
+                            Islemler(hesapno, yeniislemID);
+                            break;
+                        }
+                        case 1:
+                        {
+                            int yenihesapno;
+                            printf("[SİSTEM]: Yeni hesap numarasını giriniz: ");
+                            scanf("%d", &yenihesapno);
+                            while(yenihesapno < 10000 || yenihesapno > 99999)
+                            {
+                                printf("[HATA]: Hatalı hesap numarası girdiniz!\n");
+                                printf("[BİLGİ]: Bankamıza ait hesap numaraları 10000-99999 arasında olmalıdır!\n");
+                                printf("[SİSTEM]: Yeni hesap numarasını giriniz: ");
+                                scanf("%d", &yenihesapno);
+                            }
+                            int yeniislemID;
+                            printf("1 - Para Çekme\n");
+                            printf("2 - Para Yatırma\n");
+                            printf("3 - Para Transferi\n");
+                            printf("4 - Sistemden Çıkış\n");
+                            printf("[SİSTEM]: Yapmak istediğiniz işlemi seçiniz: ");
+                            scanf("%d", &yeniislemID);
+                            while(yeniislemID < 1 || yeniislemID > 4)
+                            {
+                                printf("[HATA]: Hatalı işlem ID girdiniz!");
+                                printf("[BİLGİ]: İşlem ID'lerimiz 1-4 arasındadır!");
+                                printf("1 - Para Çekme\n");
+                                printf("2 - Para Yatırma\n");
+                                printf("3 - Para Transferi\n");
+                                printf("4 - Sistemden Çıkış\n");
+                                printf("[SİSTEM]: Yapmak istediğiniz işlemi seçiniz: ");
+                                scanf("%d", &yeniislemID);
+                            }
+                            Islemler(yenihesapno, yeniislemID);
+                            break;
+                        }
+                    }
+                }
+            }
             break;
         }
         case 4:
